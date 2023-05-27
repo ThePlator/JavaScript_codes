368d64961311b42e2c4a4e5f3800779b08c04379
Sameer <contact.sameer04@gmail.com>
2023-05-27 19:52:48 +0530

    Data Types

---
+++ data types/script.js
@@ -0,0 +1,49 @@
+//String
+const firstName = 'sara';
+
+const output = firstName;
+
+console.log(output, typeof output);
+//Number
+
+const age = 30;
+const output1 = age;
+
+console.log(output1, typeof output1);
+
+//Boolean 
+const hasKids = true;
+
+//null 
+const aptnumber = null;
+
+const output2 = firstName;
+
+console.log(output2, typeof output2);
+
+//undefined
+
+let score;
+
+// Symbol
+
+const id = symbol('id');
+
+//BigInt
+
+const n = 87215885524454n;
+
+// Reference Types 
+const numbers =[1,2,3,4,5];
+
+const person = {
+    name : 'sameer',
+}
+
+function sayHello(){
+    console.groupCollapsed('hello');
+}
+
+const output3 = sayHello;
+
+console.log(output3, typeof output3);
