4e7e46870e33e00148aac812dd39ca15de25240f
Sameer <contact.sameer04@gmail.com>
2023-05-27 15:07:37 +0530

    Variables and constant

---
+++ Variables & Constant/script.js
@@ -0,0 +1,60 @@
+// ways to declare a variable 
+// var, let, const
+
+let firstName = 'Jhon';
+const lastName = 'Doe';
+let age = 30;
+
+console.log(age);
+
+//Naming Conventions 
+// - Only letters, numbers, underscore and dollar sings
+// - can't start with number
+
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+// We can re-assign `let` variables. If you change `age` to use `const`, you will get an error
+age = 31;
+console.log(age);
+
+// With let, we can declare a value without assigning a value
+let score;
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+// We can not re-assign a const variable
+// x = 200 // Will result in an error
+
+// We can still manipulate arrays and objects using const
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: 'Brad',
+};
+person.name = 'John';
+person.email = 'brad@gmail.com';
+console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);
