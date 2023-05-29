f7095318a7bdfcfff639d90f6838513095fac01b
Sameer <contact.sameer04@gmail.com>
2023-05-29 23:47:11 +0530

    Operators

---
+++ Variables and data type/Operators/script.js
@@ -0,0 +1,62 @@
+// Arithmetic Operators
+
+let x;
+
+x = 5+5;
+x = 5-3;
+x = 5*5;
+x = 5/2; // Float type
+x = 5%2;
+
+console.log(x);
+
+// Concatenation
+
+let y = 'Hello' + ' ' + 'World';
+
+console.log(y);
+
+// Exponent 
+ 
+let z = 2**3;
+console.log(z);
+
+// Increment 
+let a = 3;
+a++; //a = a+1;
+console.log(a);
+
+//Decrement
+let b = 3;
+b--;
+console.log(b);
+
+//Assingment operators
+let c = 2;
+c += 3;
+c -= 1;
+c *= 2;
+c **= 2;
+c %= 2;
+console.log(c);
+
+//Comparison Operators
+
+// Equal to (just value not type)
+let d = 2 == '2';
+console.log(d);
+
+// Equal to (Type and value)
+let e = 2 === '2';
+console.log(e);
+
+//Not equal to (type and value)
+let f = 2 !== '2';
+console.log(f);
+
+//Greater than and less than
+let g = 8 > 7;//true 
+g = -9 > 9;
+console.log(g);
+
+
