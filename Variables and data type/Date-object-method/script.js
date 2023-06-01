979f7fd9239bb17e6929155a3e18ade8f2770910
Sameer <contact.sameer04@gmail.com>
2023-06-01 12:16:38 +0530

    Date_Object_method

---
+++ Variables and data type/Date-object-method/script.js
@@ -0,0 +1,44 @@
+let x;
+let d = new Date();
+
+// Date method 
+
+x = d.toString();
+
+x = d.getDate();
+
+x = d.getMonth() + 1;
+
+x = d.getFullYear();
+
+x = d.getHours();
+
+x = d.getMinutes();
+
+x = d.getSeconds();
+
+x = d.getMilliseconds();
+
+x = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+
+// Intl.DateTimeFormat API (locale specific)
+
+x = Intl.DateTimeFormat('en-US').format(d);
+x = Intl.DateTimeFormat('en-GB').format(d);
+x = Intl.DateTimeFormat('default').format(d);
+
+x = Intl.DateTimeFormat('default', { month: 'long' }).format(d);
+
+x = d.toLocaleString('default', { month: 'short' });
+
+x = d.toLocaleString('default', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZone: 'America/New_York',
+});
+console.log(x);
