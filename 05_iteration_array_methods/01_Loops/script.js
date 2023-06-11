4604488b201681790725d6cfcc0c269baeb7eb46
Sameer <contact.sameer04@gmail.com>
2023-06-11 20:03:39 +0530

    Loops

---
+++ 05_iteration_array_methods/01_Loops/script.js
@@ -0,0 +1,110 @@
+// for ([initialExpression]; [conditionExpression]; [incrementExpression])
+//   statement;
+
+// INITITAL EXPRESSION - Initializes a variable/counter
+// CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
+// INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
+// STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 7) {
+//     console.log('7 is my lucky number');
+//   } else {
+//     console.log('Number ' + i);
+//   }
+// }
+
+// Nested loops
+// for (let i = 1; i <= 10; i++) {
+//   console.log('Number ' + i);
+
+//   for (let j = 1; j <= 5; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+// Loop through an array
+const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim'];
+
+for (let i = 0; i < names.length; i++) {
+  if (names[i] === 'John') {
+    console.log(names[i] + ' is the best');
+  } else {
+    console.log(names[i]);
+  }
+}
+
+// Break
+for (let i = 0; i <= 20; i++) {
+  if (i === 15) {
+    console.log('Breaking...');
+    break;
+  }
+  console.log(i);
+}
+
+// Continue
+for (let i = 0; i <= 20; i++) {
+  if (i === 13) {
+    console.log('Skipping 13...');
+    continue;
+  }
+  console.log(i);
+}
+
+let i = 0;
+
+while (i <= 20) {
+  console.log('Number ' + i);
+  i++;
+}
+
+// Loop over arrays
+const arr = [10, 20, 30, 40, 50];
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
+// Nesting while loops
+while (i <= 5) {
+  console.log('Number ' + i);
+
+  let j = 1;
+  while (j <= 5) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    j++;
+  }
+
+  i++;
+}
+
+// Do While Loop - Always runs once
+do {
+  console.log('Number ' + i);
+  i++;
+} while (i <= 20);
+
+/* Instructions:
+
+Print/log the numbers from 1 to 100
+For multiples of three print "Fizz" instead of the number
+For multiples of five print "Buzz"
+For numbers which are multiples of both three and five print "FizzBuzz".
+Hints:
+
+If you have taken the course up to this point, then you know how to write a loop and output/log something for each iteration. You also know how to check for a condition with "if/else/else if". You also know how to get a remainder of a number using the modulus operator (%). This is all you need to know to complete this challenge. Good luck!*/
+
+// solution:->
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 == 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 == 0) {
+    console.log('Fizz');
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
