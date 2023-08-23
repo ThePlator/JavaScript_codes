90bb4e311433a95c428ed4cadbe9c6fe2cc176b2
Sameer <contact.sameer04@gmail.com>
2023-08-23 12:29:40 +0530

    functional setinterval fxn

--- 08_Asynchronous_javascript/02_setinterval-clearinterval/script.js
+++ 08_Asynchronous_javascript/02_setinterval-clearinterval/script.js
@@ -9,7 +9,6 @@ let intervalID;
 function startChange() {
   if (!intervalID) {
     intervalID = setInterval(changeRandomColor, 1000);
-    console.log('hello');
   }
 }
 
@@ -17,3 +16,10 @@ function changeRandomColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = `#${randomColor}`;
 }
+
+function stopChange() {
+  clearInterval(intervalID);
+}
+
+document.getElementById('start').addEventListener('click', startChange);
+document.getElementById('stop').addEventListener('click', stopChange);
